21f12e9d761ff4ad0fdf0149227640ec8f0b3904
Ben Moon <software@guiltydolphin.com>
2022-07-12 12:15:13 +0100

    Fix table header being hidden with active search

--- shared/js/devtools/panel.es6.js
+++ shared/js/devtools/panel.es6.js
@@ -265,6 +265,6 @@ tdsOption.addEventListener('change', (e) => {
 
 displayFilters.forEach((input) => {
     input.addEventListener('change', () => {
-        document.querySelectorAll('tr').forEach(setRowVisible)
+        document.querySelectorAll('tbody > tr').forEach(setRowVisible)
     })
 })
