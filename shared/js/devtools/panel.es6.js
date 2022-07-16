c69fd62518c47bbb6c88e0f9f060b610c53d0aea
Ben Moon <software@guiltydolphin.com>
2022-07-16 11:37:52 +0100

    Add on/off colouring for protection buttons

--- shared/js/devtools/panel.es6.js
+++ shared/js/devtools/panel.es6.js
@@ -47,6 +47,17 @@ const actionIcons = {
     ignore: '⚠️'
 }
 
+/**
+ * @param {HTMLElement} element
+ * @param {string} textName
+ * @param {boolean} isEnabled
+ */
+function setupProtectionButton(element, textName, isEnabled) {
+    element.innerText = `${textName}: ${isEnabled ? 'ON' : 'OFF'}`
+    element.classList.add(`protection-button-${isEnabled ? 'on' : 'off'}`)
+    element.classList.remove(`protection-button-${isEnabled ? 'off' : 'on'}`)
+}
+
 const actionHandlers = {
     tracker: (m) => {
         const { tracker, url, requestData, siteUrl } = m.message
@@ -79,10 +90,13 @@ const actionHandlers = {
     },
     tabChange: (m) => {
         const tab = m.message
-        protectionButton.innerText = `Protection: ${tab.site?.allowlisted || tab.site?.isBroken ? 'OFF' : 'ON'}`
+        const protectionDisabled = tab.site?.allowlisted || tab.site?.isBroken
+        setupProtectionButton(protectionButton, 'Protection', !protectionDisabled)
         loadConfigurableFeatures.then((features) => {
             features.forEach((feature) => {
-                document.getElementById(feature).innerText = `${feature}: ${tab.site?.enabledFeatures.includes(feature) ? 'ON' : 'OFF'}`
+                const featureEnabled = tab.site?.enabledFeatures.includes(feature)
+                const featureButton = document.getElementById(feature)
+                setupProtectionButton(featureButton, feature, featureEnabled)
             })
         })
     },
